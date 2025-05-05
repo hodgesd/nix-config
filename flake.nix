b35f888c49bd85ec4279a983488a0f19df65d6b8
hodgesd <hodgesd@gmail.com>
2025-05-05 01:28:12 -0500

    Remove sops-nix from flake inputs
    The sops-nix input was removed as it is no longer required. This simplifies the flake configuration, reducing unnecessary dependencies.

--- flake.nix
+++ flake.nix
@@ -17,8 +17,6 @@
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
 
 
