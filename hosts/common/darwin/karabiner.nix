302a7830ad711cdd288627fa0d595da0021f9d83
hodgesd <hodgesd@gmail.com>
2025-12-01 18:17:17 -0600

    Enhance Karabiner config: add `virtual_hid_keyboard` settings and improve maintainability.

--- hosts/common/darwin/karabiner.nix
+++ hosts/common/darwin/karabiner.nix
@@ -5,36 +5,39 @@
   home-manager.users.hodgesd = {
     xdg.configFile."karabiner/karabiner.json" = {
       text = ''
-        {
-          "global": { "check_for_updates_on_startup": true },
-          "profiles": [
-            {
-              "name": "Default",
-              "selected": true,
-              "complex_modifications": {
-                "rules": [
-                  {
-                    "description": "Caps Lock: tap = toggle, hold = meh",
-                    "manipulators": [
-                      {
-                        "from": { "key_code": "caps_lock" },
-                        "to": [{
-                          "key_code": "left_shift",
-                          "modifiers": ["left_control", "left_option"]
-                        }],
-                        "to_if_alone": [{
-                          "key_code": "caps_lock",
-                          "hold_down_milliseconds": 200
-                        }],
-                        "type": "basic"
-                      }
-                    ]
-                  }
-                ]
+          {
+            "global": { "check_for_updates_on_startup": true },
+            "profiles": [
+              {
+                "name": "Default",
+                "selected": true,
+                "virtual_hid_keyboard": {
+                "keyboard_type": "ansi"
+                },
+                "complex_modifications": {
+                  "rules": [
+                    {
+                      "description": "Caps Lock: tap = toggle, hold = meh",
+                      "manipulators": [
+                        {
+                          "from": { "key_code": "caps_lock" },
+                          "to": [{
+                            "key_code": "left_shift",
+                            "modifiers": ["left_control", "left_option"]
+                          }],
+                          "to_if_alone": [{
+                            "key_code": "caps_lock",
+                            "hold_down_milliseconds": 200
+                          }],
+                          "type": "basic"
+                        }
+                      ]
+                    }
+                  ]
+                }
               }
-            }
-          ]
-        }
+            ]
+          }
       '';
       force = true;
     };
