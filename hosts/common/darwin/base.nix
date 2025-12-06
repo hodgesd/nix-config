f0d463a7d3f1cc46e1b331f0f3afbe8c00a9050b
hodgesd <hodgesd@gmail.com>
2025-12-06 06:24:20 -0600

    Enhance macOS configurations: add build optimizations, better caching, automatic garbage collection, and store optimization; include system activation script for reloading settings without reboot.

--- hosts/common/darwin/base.nix
+++ hosts/common/darwin/base.nix
@@ -7,8 +7,33 @@
   users.users.${config.majordouble.user}.home = "/Users/${config.majordouble.user}";
 
   nix = {
-    settings.warn-dirty = false;
+    settings = {
+      warn-dirty = false;
+      # Build optimization
+      max-jobs = "auto";
+      cores = 0;
+      # Better caching
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     channel.enable = false;
+
+    # Automatic garbage collection
+    gc = {
+      automatic = true;
+      interval.Day = 7;
+      options = "--delete-older-than 30d";
+    };
+
+    # Automatic store optimization
+    optimise.automatic = true;
   };
 
   system.stateVersion = 5;
@@ -23,6 +48,7 @@
   system.primaryUser = config.majordouble.user;
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  # reloads system settings without requiring a reboot
   system.activationScripts.activateSettings.text = ''
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
   '';
