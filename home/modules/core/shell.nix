09c8a8a43454d1fde1b2b352e40c1be19ddad063
hodgesd <hodgesd@gmail.com>
2025-12-05 22:07:47 -0600

    Enhance `shell.nix`: add `shellAliases`, `initExtra`, environment variables, and PATH configurations; remove unused `mac-dot-zshrc`.

--- home/modules/core/shell.nix
+++ home/modules/core/shell.nix
@@ -9,13 +9,42 @@
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
-    initContent = ''
-      ${builtins.readFile ../../../data/mac-dot-zshrc}
 
-      # Initialize zoxide
-      eval "$(${pkgs.zoxide}/bin/zoxide init zsh --cmd cd)"
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
+      "......" = "cd ../../../../..";
+      "......." = "cd ../../../../../..";
+      "........" = "cd ../../../../../../..";
+    };
+
+    initExtra = ''
+      # Comma function for nix-index
+      , () {
+        nix run nixpkgs#comma -- "$@"
+      }
+
+      # ASCII art banner
+      ${pkgs.figurine}/bin/figurine -f "Doom.flf" mbp
     '';
   };
+
+  # Environment variables
+  home.sessionVariables = {
+    EDITOR = "micro";
+    TERM = "xterm-256color";
+    LLM_MODEL = "m7b";
+    CLICOLOR = "1";
+    LSCOLORS = "gxfxcxdxbxgggdabagacad";
+  };
+
+  # PATH additions
+  home.sessionPath = [
+    "/opt/homebrew/bin"
+    "$HOME/go/bin"
+  ];
 
   programs.bash.enable = true;
 }
