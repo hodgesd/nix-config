afc5e2b79a38f1d32f077d4a2cb8ab3c85d31e70
hodgesd <hodgesd@gmail.com>
2025-12-05 23:24:37 -0600

    Replace `nixpkgs-darwin` with `nixpkgs` in `flake.lock` and `flake.nix` for consistency and streamline inputs configuration.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nix-darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -15,33 +14,27 @@
     };
   };
 
-  outputs = {self, ...} @ inputs:
+  outputs = { self, ... } @ inputs:
     with inputs; let
       inherit (self) outputs;
 
       stateVersion = "25.05";
-      libx = import ./lib {inherit inputs outputs stateVersion;};
-    in {
+      libx = import ./lib { inherit inputs outputs stateVersion; };
+    in
+    {
       darwinConfigurations = {
-        mbp = libx.mkDarwin {hostname = "mbp";};
-        mini = libx.mkDarwin {hostname = "mini";};
-        air = libx.mkDarwin {hostname = "air";};
+        mbp = libx.mkDarwin { hostname = "mbp"; };
+        mini = libx.mkDarwin { hostname = "mini"; };
+        air = libx.mkDarwin { hostname = "air"; };
       };
 
       nixosConfigurations = {
-        desktop = libx.mkNixos {hostname = "desktop";};
-        ktz-cloud = libx.mkNixos {hostname = "ktz-cloud";};
-        morphnix = libx.mkNixos {hostname = "morphnix";};
-        nix-dev = libx.mkNixos {hostname = "nix-dev";};
-        nvllama = libx.mkNixos {hostname = "nvllama";};
-
-        nixos-air = libx.mkNixos {hostname = "nixos-air";};
-      };
-
-      formatter = {
-        aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
-        x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.alejandra;
-        x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+        desktop = libx.mkNixos { hostname = "desktop"; };
+        ktz-cloud = libx.mkNixos { hostname = "ktz-cloud"; };
+        morphnix = libx.mkNixos { hostname = "morphnix"; };
+        nix-dev = libx.mkNixos { hostname = "nix-dev"; };
+        nvllama = libx.mkNixos { hostname = "nvllama"; };
+        nixos-air = libx.mkNixos { hostname = "nixos-air"; };
       };
     };
 }
